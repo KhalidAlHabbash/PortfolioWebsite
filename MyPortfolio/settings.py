07ab248f08af46e6a049bf08ab2b9089df045e24
Khalid AlHabbash <khalidhabbash987@gmail.com>
2020-07-22 14:41:51 -0700

    blog and work and media add

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Work.apps.WorkConfig',
+    'Blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
